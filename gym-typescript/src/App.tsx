88d88833c4f2864d35d68ca908af968b330d8ad1
Yu Hang Lee <lyu8572@gmail.com>
2024-09-02 21:16:36 +0800

    Add Navbar

--- gym-typescript/src/App.tsx
+++ gym-typescript/src/App.tsx
@@ -1,3 +1,6 @@
+import Navbar from "@/scenes/navbar"
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+
+  return <h1 className="app bg-gray-20"><Navbar/></h1>;
 }
