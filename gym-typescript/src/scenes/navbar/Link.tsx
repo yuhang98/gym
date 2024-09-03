d9fafe622143416aacae926dd9f5bddc6dcfcaba
Yu Hang Lee <lyu8572@gmail.com>
2024-09-03 21:00:00 +0800

    Navbar and Link

---
+++ gym-typescript/src/scenes/navbar/Link.tsx
@@ -0,0 +1,23 @@
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+
+type Props = {
+  page: string;
+};
+
+const Link = ({ page }: Props) => {
+  const lowerCasePage = page.toLowerCase().replace(/ /g, '');
+  return (
+    <AnchorLink
+      className={`${selectedPage === lowerCasePage ? 'text-primary-500' : ''}
+      transition duration-500 hover:text-primary-300
+      `}
+      href={`#${lowerCasePage}`}
+      //   onClick={}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
+
+export default Link;
+// 1
