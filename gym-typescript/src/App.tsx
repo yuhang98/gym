efd504fa9f5641dcdbe91b99d5df6442d2301f86
Yu Hang Lee <lyu8572@gmail.com>
2024-10-01 18:47:11 +0800

    Add Home Page

--- gym-typescript/src/App.tsx
+++ gym-typescript/src/App.tsx
@@ -1,15 +1,36 @@
 import Navbar from '@/scenes/navbar';
-import { useState } from 'react';
+import Home from '@/scenes/home';
+import { useEffect, useState } from 'react';
 import { SelectedPage } from './shared/types';
 
 export default function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home
   );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) {
+        setIsTopOfPage(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <h1 className="app bg-gray-20">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+        isTopOfPage={isTopOfPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
     </h1>
   );
 }
-//
--- gym-typescript/src/scenes/navbar/index.tsx
+++ gym-typescript/src/scenes/navbar/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Bar3Icon, Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 import Logo from '@/assets/Logo.png';
 import Link from './Link';
@@ -9,15 +9,20 @@ import ActionButton from '@/shared/ActionButton';
 type Props = {
   selectedPage: SelectedPage;
   setSelectedPage: (value: SelectedPage) => void;
+  isTopOfPage: boolean;
 };
 
-const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+const Navbar = ({ selectedPage, setSelectedPage, isTopOfPage }: Props) => {
   const flexBetween = 'flex items-center justify-between';
   const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
   const isAboveMediumScreens = useMediaQuery('(min-width:1060px');
+  const navbarBackground = isTopOfPage ? '' : 'bg-primary-100 drop-shadow';
+
   return (
     <nav>
-      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div
+        className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}
+      >
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
             {/* LEFT SIDE */}
@@ -107,4 +112,3 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
 };
 
 export default Navbar;
-// 1 37 45
