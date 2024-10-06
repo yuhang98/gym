5da6c2c93e45180d1134f0b0bea32da072921ee9
Yu Hang Lee <lyu8572@gmail.com>
2024-10-06 15:47:26 +0800

    Complete OurClasses Page

--- gym-typescript/src/App.tsx
+++ gym-typescript/src/App.tsx
@@ -1,6 +1,8 @@
 import Navbar from '@/scenes/navbar';
 import Home from '@/scenes/home';
 import Benefits from '@/scenes/benefits';
+import OurClasses from '@/scenes/ourClasses';
+import ContactUs from '@/scenes/contactUs';
 import { useEffect, useState } from 'react';
 import { SelectedPage } from './shared/types';
 
@@ -33,6 +35,8 @@ export default function App() {
       />
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
     </h1>
   );
 }
---
+++ gym-typescript/src/scenes/contactUs/index.tsx
@@ -0,0 +1,13 @@
+import { SelectedPage } from '@/shared/types';
+import React from 'react';
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const ContactUs = ({ setSelectedPage }: Props) => {
+  return <div>ContactUs</div>;
+};
+
+export default ContactUs;
+// 3 23 18
